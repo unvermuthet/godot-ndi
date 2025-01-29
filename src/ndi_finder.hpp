6c870705e7ad2f3b84e331491e34df690c8be497
unvermuthet <40361609+unvermuthet@users.noreply.github.com>
2025-01-29 12:26:36 +0100

    remove old include of ndifind

--- src/ndi_finder.hpp
+++ src/ndi_finder.hpp
@@ -16,47 +16,47 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #include <godot_cpp/classes/semaphore.hpp>
 #include <godot_cpp/classes/hashing_context.hpp>
 #include "ndi.hpp"
-#include "ndi_find.hpp"
+#include "video_stream_ndi.hpp"
 
 using namespace godot;
 
 class NDIFinder : public Node {
-    GDCLASS(NDIFinder, Node)
+	GDCLASS(NDIFinder, Node)
 
-    NDIlib_find_create_t find_desc;
-    CharString groups;
-    CharString extra_ips;
+	NDIlib_find_create_t find_desc;
+	CharString groups;
+	CharString extra_ips;
 
-    Ref<Thread> thr;
-    Ref<Mutex> mtx;
-    Ref<Semaphore> sem;
+	Ref<Thread> thr;
+	Ref<Mutex> mtx;
+	Ref<Semaphore> sem;
 
-    bool exit_thread = false;
-    bool rebuild_find = false;
+	bool exit_thread = false;
+	bool rebuild_find = false;
 
-    void process_thread();
+	void process_thread();
 
-    TypedArray<VideoStreamNDI> mtx_sources;
+	TypedArray<VideoStreamNDI> mtx_sources;
 
-    protected:
-        static void _bind_methods();
+	protected:
+		static void _bind_methods();
 
-    public:
-        NDIFinder();
-        ~NDIFinder();
+	public:
+		NDIFinder();
+		~NDIFinder();
 
-        void _ready() override;
-        void _exit_tree() override;
-        void _process(double delta) override;
+		void _ready() override;
+		void _exit_tree() override;
+		void _process(double delta) override;
 
-        TypedArray<VideoStreamNDI> get_sources() const;
+		TypedArray<VideoStreamNDI> get_sources() const;
 
-        void set_show_local_sources(const bool state);
-        bool get_show_local_sources() const;
+		void set_show_local_sources(const bool state);
+		bool get_show_local_sources() const;
 
-        void set_groups(const PackedStringArray groups);
-        PackedStringArray get_groups() const;
+		void set_groups(const PackedStringArray groups);
+		PackedStringArray get_groups() const;
 
-        void set_extra_ips(const PackedStringArray extra_ips);
-        PackedStringArray get_extra_ips() const;
+		void set_extra_ips(const PackedStringArray extra_ips);
+		PackedStringArray get_extra_ips() const;
 };
