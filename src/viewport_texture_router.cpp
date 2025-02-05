4f5d02ca366906925b02e7a4b5385fc2e6fda8f0
unvermuthet <40361609+unvermuthet@users.noreply.github.com>
2025-02-05 23:24:39 +0000

    Add MPL license header

--- src/viewport_texture_router.cpp
+++ src/viewport_texture_router.cpp
@@ -1,3 +1,12 @@
+/*
+https://github.com/unvermuthet/godot-ndi
+		(C) 2025 Henry Muth - unvermuthet
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #include "includes.hpp"
 
 ViewportTextureRouter::ViewportTextureRouter() {
