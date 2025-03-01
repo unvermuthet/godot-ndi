22980e54a9e23658b636b38ccb5baf9eb450d8c7
unvermuthet <40361609+unvermuthet@users.noreply.github.com>
2025-03-01 12:59:56 +0100

    test: NDIFinder discovers grouped source from NDIOutput

--- project/test/integration/test_finder.gd
+++ project/test/integration/test_finder.gd
@@ -1,16 +1,15 @@
 extends GutTest
 
 func test_finds_grouped_source():
+	var output: NDIOutput = add_child_autoqfree(NDIOutput.new())
+	output.name = "GodotSource"
+	output.groups = ["GodotGroup"]
+
 	var finder: NDIFinder = add_child_autoqfree(NDIFinder.new())
 	finder.show_local_sources = true
 	finder.groups = ["GodotGroup"]
 
-	# var output: NDIOutput = add_child_autoqfree(NDIOutput.new())
-	# output.name = "GodotSource"
-	# output.groups = ["GodotGroup"]
+	await wait_for_signal(finder.sources_changed, 10, "Waiting for source to be found")
 
-	# await wait_for_signal(finder.sources_changed, 10, "Waiting for source to be found")
-
-	# assert_eq(finder.get_sources().size(), 1, "One source found")
-	# assert_string_ends_with(finder.get_sources()[0].name, "(GodotSource (Game))", "Correct source found")
-	pass_test("")
+	assert_eq(finder.get_sources().size(), 1, "One source found")
+	assert_string_ends_with(finder.get_sources()[0].name, " (GodotSource (Game))", "Correct source found")
